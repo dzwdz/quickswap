56fc9a7b6e5483cc45f70edbdd168b91594f83ee
Dźwiedź <kg67199@gmail.com>
2020-08-13 19:37:13 +0200

    0.1.2

--- src/main/java/dzwdz/quickswap/QuickSwap.java
+++ src/main/java/dzwdz/quickswap/QuickSwap.java
@@ -7,30 +7,56 @@ import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 public class QuickSwap implements ClientModInitializer {
-	private KeyBinding[] keysQS;
-	private boolean[] lastState = {false, false, false, false, false, false, false, false, false};
-	private int lastSlot;
+    private KeyBinding[] hotbarBinds;
+    private boolean[] lastHotbarState = {false, false, false, false, false, false, false, false, false};
+    private int lastSlot;
 
-	@Override
-	public void onInitializeClient() {
-		keysQS = new KeyBinding[9];
+    private KeyBinding[] camBinds;
+    private boolean[] lastCamState = {false, false, false};
+    private int lastCam;
 
-		for (int i = 0; i < 9; i++) {
-			keysQS[i] = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.dzwdz.quickswap.qs."+Integer.toString(i+1), InputUtil.UNKNOWN_KEY.getCode(), "key.categories.inventory"));
-		}
+    @Override
+    public void onInitializeClient() {
+        hotbarBinds = new KeyBinding[9];
+        for (int i = 0; i < 9; i++) {
+            hotbarBinds[i] = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.dzwdz.quickswap.qs."+Integer.toString(i+1),
+                                                                                InputUtil.UNKNOWN_KEY.getCode(),
+                                                                                "key.categories.dzwdz.quickswap"));
+        }
 
-		ClientTickCallback.EVENT.register(client -> {
-			for(int i = 0; i < 9; i++) {
-				if (this.keysQS[i].isPressed() != lastState[i]) {
-					if (this.keysQS[i].isPressed()) {
-						lastSlot = client.player.inventory.selectedSlot;
-						client.player.inventory.selectedSlot = i;
-					} else {
-						client.player.inventory.selectedSlot = lastSlot;
-					}
-					lastState[i] ^= true;
-				}
-			}
-		});
-	}
+        camBinds = new KeyBinding[3];
+        for (int i = 0; i < 3; i++) {
+            camBinds[i] = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.dzwdz.quickswap.cam."+Integer.toString(i+1),
+                                                                            InputUtil.UNKNOWN_KEY.getCode(),
+                                                                            "key.categories.dzwdz.quickswap"));
+        }
+
+        ClientTickCallback.EVENT.register(client -> {
+            // handle hotbar swaps
+            for (int i = 0; i < 9; i++) {
+                if (this.hotbarBinds[i].isPressed() != lastHotbarState[i]) {
+                    if (this.hotbarBinds[i].isPressed()) {
+                        lastSlot = client.player.inventory.selectedSlot;
+                        client.player.inventory.selectedSlot = i;
+                    } else {
+                        client.player.inventory.selectedSlot = lastSlot;
+                    }
+                    lastHotbarState[i] ^= true;
+                }
+            }
+
+            // handle perspective swaps
+            for (int i = 0; i < 3; i++) {
+                if (this.camBinds[i].isPressed() != lastCamState[i]) {
+                    if (this.camBinds[i].isPressed()) {
+                        lastCam = client.options.perspective;
+                        client.options.perspective = i;
+                    } else {
+                        client.options.perspective = lastCam;
+                    }
+                    lastCamState[i] ^= true;
+                }
+            }
+        });
+    }
 }
